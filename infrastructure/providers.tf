7f44844201f79fe272dd0c15ef25df5d06f8eeb2
Antonio Berbece <razvan.berbece@hotmail.com>
2024-07-06 23:22:18 +0100

    fix TF deploy flow (1)

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "5.36.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.36.0"
+    }
   }
 }
 
@@ -13,4 +17,8 @@ provider "google" {
   zone    = var.PROJECT_ZONE
 
   credentials = var.GCP_CREDENTIALS
+}
+
+provider "google-beta" {
+  project = var.PROJECT_ID
 }
