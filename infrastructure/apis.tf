144a7b3beaabd1368f961a6f328a676db129769e
Antonio Berbece <razvan.berbece@hotmail.com>
2024-07-06 23:11:53 +0100

    enables billing API

--- infrastructure/apis.tf
+++ infrastructure/apis.tf
@@ -1,7 +1,8 @@
 resource "google_project_service" "apis" {
   for_each = toset([
     "cloudbilling.googleapis.com",
-    "iam.googleapis.com"
+    "iam.googleapis.com",
+    "billingbudgets.googleapis.com"
   ])
 
   service            = each.value
