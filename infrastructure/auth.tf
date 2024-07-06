5b882c42af0a1050707092befc8d3dd620485a17
Antonio Berbece <razvan.berbece@hotmail.com>
2024-07-06 23:29:10 +0100

    fix TF deploy flow (2)

--- infrastructure/auth.tf
+++ infrastructure/auth.tf
@@ -4,9 +4,17 @@ resource "google_service_account" "github-tf-manager-service-account" {
   display_name = "TF Manager (GitHub Access)"
 }
 
-resource "google_service_account_iam_binding" "github-tf-manager-service-account-iam" {
+resource "google_service_account_iam_binding" "github-tf-manager-service-account-owner-iam" {
   service_account_id = google_service_account.github-tf-manager-service-account.name
   role               = "roles/owner"
+  members = [
+    "serviceAccount:${google_service_account.github-tf-manager-service-account.email}",
+  ]
+}
+
+resource "google_service_account_iam_binding" "github-tf-manager-service-account-billing-iam" {
+  service_account_id = google_service_account.github-tf-manager-service-account.name
+  role               = "roles/billing.admin"
   members = [
     "serviceAccount:${google_service_account.github-tf-manager-service-account.email}",
   ]
