377e01b4dae95a24d942804fef60547593769a8f
Antonio Berbece <razvan.berbece@hotmail.com>
2024-07-06 22:19:17 +0100

    move terraform block in providers

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.36.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.PROJECT_ID
   region  = var.PROJECT_REGION
