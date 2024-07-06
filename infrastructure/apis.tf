0a977294b2778d1c097d04f33244d7ffee34f3e5
Antonio Berbece <razvan.berbece@hotmail.com>
2024-07-06 23:40:46 +0100

    enables more googleapis

--- infrastructure/apis.tf
+++ infrastructure/apis.tf
@@ -2,7 +2,9 @@ resource "google_project_service" "apis" {
   for_each = toset([
     "cloudbilling.googleapis.com",
     "iam.googleapis.com",
-    "billingbudgets.googleapis.com"
+    "billingbudgets.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "compute.googleapis.com"
   ])
 
   service            = each.value
