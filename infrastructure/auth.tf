d4a0871c11732c664dcc5d09d47592dc408e0c5b
Antonio Berbece <razvan.berbece@hotmail.com>
2024-07-07 02:47:03 +0100

    adds workload id pool for auth

--- infrastructure/auth.tf
+++ infrastructure/auth.tf
@@ -4,10 +4,39 @@ resource "google_service_account" "github-tf-manager-service-account" {
   display_name = "TF Manager (GitHub Access)"
 }
 
+resource "google_service_account" "github-webapp-manager-service-account" {
+  project      = var.PROJECT_ID
+  account_id   = "gh-webapp-manager"
+  display_name = "Webapp Manager (GitHub Access)"
+}
+
 resource "google_service_account_iam_binding" "github-tf-manager-service-account-owner-iam" {
   service_account_id = google_service_account.github-tf-manager-service-account.name
   role               = "roles/owner"
   members = [
     "serviceAccount:${google_service_account.github-tf-manager-service-account.email}",
   ]
+}
+
+resource "google_service_account_iam_binding" "github-webapp-manager-service-account-owner-iam" {
+  service_account_id = google_service_account.github-webapp-manager-service-account.name
+  role               = "roles/owner"
+  members = [
+    "serviceAccount:${google_service_account.github-webapp-manager-service-account.email}",
+  ]
+}
+
+module "gh_oidc" {
+  source            = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  version           = "v3.1.2"
+  project_id        = var.PROJECT_ID
+  pool_id           = "cd-identity-pool"
+  pool_display_name = "CD Identity Pool"
+  provider_id       = "cd-provider-antdev"
+  sa_mapping = {
+    (google_service_account.github-webapp-manager-service-account.account_id) = {
+      sa_name   = google_service_account.github-webapp-manager-service-account.name
+      attribute = "*"
+    }
+  }
 }
